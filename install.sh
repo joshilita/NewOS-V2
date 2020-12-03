5d9f27b0f8b9d006232ed55bf7f88ee35fd12b6d
joshilita <52619112+joshilita@users.noreply.github.com>
2020-12-03 11:01:35 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+GREENFG="\e[38;5;82m"
+REDWEAKFG="\e[38;5;132m"
+BBLUEFG="\e[1;38;5;75m"
+GBLUEFG="\e[1;38;5;73m"
+FOLDERNAMEFG="\e[1;38;5;45m"
+ERRORFG="\e[1;38;5;197m"
+RESET="\e[0m"
+BOLD="\e[1m"
+
+#start
+echo "${GREENFG}Welcome to the new and improved version of NewOS! This script will install the OS for you. Enjoy!"
